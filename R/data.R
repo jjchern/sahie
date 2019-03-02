2da0b5d2d46cb2a25bdd2aaf947a8c8e1352d59f
jjchern <jiajia.chern@gmail.com>
2019-03-02 15:19:36 -0600

    Document county_a5064

--- R/data.R
+++ R/data.R
@@ -1,3 +1,9 @@
 #' @title County-Level Number of Uninsured Eestimates
 #' @description Use \code{sahie_county}.
 "sahie_county"
+
+#' County-Level Estimates of Health Insurance Coverage for Age 50-64
+#'
+#' Available years: 2008-2016. Estimates are specific to those age 50-64,
+#' all race, both sexes, and for all income levels.
+"county_a5064"
